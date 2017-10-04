9cefba4f331ded6379bbc1566d5488f6b0ff3d30
Walig <c.walig@gmail.com>
2017-10-05 10:37:41 +1300

    Add Services

---
+++ dependencies/Services.swift
@@ -0,0 +1,21 @@
+//
+//  Services.swift
+//  
+//
+//  Created by Walig Castain on 05/10/17.
+//  Copyright © 2017 PaperKite. All rights reserved.
+//
+
+struct Services {
+        
+    // /// The dataservice object
+    // let dataService: DataService
+    
+    // // Struct to provide the rx namespace for the custom reactive Services extensions.
+    // let rx: RxServices
+    
+    // init(dataService: DataService) {
+    //     self.dataService = dataService
+    // }
+
+}
